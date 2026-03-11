884853a40c3d644b051c50ad0e033699f4e7990b
Jenil Gadhiya <jenilgadhiya@Jenils-MacBook-Air.local>
2026-03-11 14:01:07 +0530

    Fix Vercel 404 for order actions

---
+++ api/orders/[id].js
@@ -0,0 +1,10 @@
+import { handleApiRequest } from "../../server/apiServer.js";
+
+export const config = {
+  runtime: "nodejs",
+};
+
+export default async function handler(req, res) {
+  return handleApiRequest(req, res);
+}
+
