bed0399337872cc998fd8e1370cb9a20d32463ba
Jenil Gadhiya <jenilgadhiya@Jenils-MacBook-Air.local>
2026-03-16 09:14:04 +0530

    fix: consolidate API routes into single serverless function

--- api/users/login.js
+++
@@ -1,8 +0,0 @@
-import { handleApiRequest } from "../../server/apiServer.js";
-
-export const config = { runtime: "nodejs" };
-
-export default async function handler(req, res) {
-  return handleApiRequest(req, res);
-}
-
